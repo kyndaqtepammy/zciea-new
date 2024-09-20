b46a77d52e49ed0f2fad169c6c10559cdca3c920
Pamela Sillah <pamsillah@yahoo.co.uk>
2024-09-20 15:44:19 +0200

    make app a PWA

--- next.config.js
+++ next.config.js
@@ -1,9 +1,15 @@
 /** @type {import('next').NextConfig} */
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+});
+
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["api.zciea.trade", "localhost"],
+    domains: ['api.zciea.trade', 'localhost'],
   },
 };
 
-module.exports = nextConfig;
+module.exports = withPWA(nextConfig);
